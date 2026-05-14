50cd49c3fee0b10027263e97d1514de3a59bd9a8
Camia <138741180+chm-0809@users.noreply.github.com>
2026-05-14 14:43:01 +0000

    Punto 4

--- parcial1/soluciones_p1.py
+++ parcial1/soluciones_p1.py
@@ -11,11 +11,25 @@ def f2(L):
             total += L[i]+(x**i)
         return total
     return f 
+
 def f3(x0, y0):
     def recta(x):
         return 2 * x - 2 * x0 + y0
     def paralela(x):
        return 2 * x - 1
     return recta, paralela
+
+def f4(L):
+    total = 0
+    for n in L:
+        if n > 10:
+            digitos = str(n)
+            primer_digito = int(digitos[0])
+            ultimo_digito  = int(digitos[-1])
+            if (primer_digito + ultimo_digito) % 2 == 0:
+                total += n
+    return total
+
+
 
     
